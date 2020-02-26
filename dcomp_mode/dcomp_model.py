8b7724c90b191b76fcf36542990a3f4530a2c802
Jason A. Tran <jasontra@usc.edu>
2020-02-25 22:35:28 -0800

    reorganize directories, rename files, update readmes, format files

--- dcomp_mode/dcomp_model.py
+++
@@ -1,9 +0,0 @@
-import mergexp as mx
-
-net = mx.Topology('hello')
-
-# replace me with your experiment topology
-nodes = [net.device(name) for name in ['master', 'n1', 'n2']]
-net.connect(nodes)
-
-mx.experiment(net)
